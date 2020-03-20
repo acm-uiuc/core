4e24d8a994ea5ae2a144aede52a90481e1e0fe76
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-20 09:20:56 -0500

    Add gitstore

---
+++ gitstore/interface.go
@@ -0,0 +1,36 @@
+package gitstore
+
+import (
+	"fmt"
+
+	"github.com/acm-uiuc/core/config"
+)
+
+type GitStore interface {
+	ParseInto(path string, out interface{}) error
+}
+
+var gs GitStore
+
+func New() (GitStore, error) {
+	if gs == nil {
+		basePath, err := config.GetConfigValue("GITSTORE_BASE_PATH")
+		if err != nil {
+			return nil, fmt.Errorf("failed to get config value: %w", err)
+		}
+
+		store := &gitStoreImpl{
+			basePath: basePath,
+			data:     map[string]*gitStoreBlob{},
+		}
+
+		err = store.refreshData()
+		if err != nil {
+			return nil, fmt.Errorf("failed to refresh store data: %w", err)
+		}
+
+		gs = store
+	}
+
+	return gs, nil
+}
