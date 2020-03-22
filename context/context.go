d6bb7ba6bbde5ae5ac71d32756aad5d01191a9cb
Arnav Sankaran <arnavsankaran@gmail.com>
2020-03-22 08:25:07 -0500

    Add render error to context

--- context/context.go
+++ context/context.go
@@ -1,6 +1,8 @@
 package context
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo"
 )
 
@@ -8,4 +10,23 @@ type Context struct {
 	echo.Context
 	Username string
 	LoggedIn bool
+}
+
+func (ctx *Context) RenderError(code int, title string, message string, err error) error {
+	params := struct {
+		Authenticated bool
+		Title         string
+		Message       string
+	}{
+		Authenticated: ctx.LoggedIn,
+		Title:         title,
+		Message:       message,
+	}
+
+	rerr := ctx.Render(code, "error", params)
+	if rerr != nil {
+		return fmt.Errorf("failed to render error: %w, original error: %w", rerr, err)
+	}
+
+	return err
 }
