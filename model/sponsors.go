afa916e0aa61ab6f315f8c0b9c98906f2b402290
Arnav Sankaran <arnavsankaran@gmail.com>
2020-05-29 15:34:00 -0500

    Template get involved sponsor section

--- model/sponsors.go
+++ model/sponsors.go
@@ -1,8 +1,9 @@
 package model
 
 type Sponsors struct {
-	Pitch    string               `yaml:"pitch"`
-	Packages []SponsorshipPackage `yaml:"packages"`
+	Pitch       string               `yaml:"pitch"`
+	GetInvolved string               `yaml:"getInvolved"`
+	Packages    []SponsorshipPackage `yaml:"packages"`
 }
 
 type SponsorshipPackage struct {
