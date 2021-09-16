1cf7e7d4fd8434d03c8e462833cf938b6b9a3fe6
Chaitanya Sood <chaitanyasood01@gmail.com>
2021-09-16 02:51:05 -0500

    Further updated sponsors page (#118)
    * added ACM Calendar
    
    * added newsletter button in header
    
    * updated officer board emails and some sigs
    
    * corrected group name top 4 to officer board
    
    * updated about page info
    
    * added line breaks
    
    * changed back about yaml name to Top4 for compliance with auth requirements
    
    * changed officer board to Top4
    
    * added newsletter to sidebar, added LinkTree to SIGs page
    
    * removed inactive SIGS
    
    * added new information git SIGGRAPH
    
    * removed Outreach, corrected mailto tag errors, replaced Infrastructure with Admin temporarily
    
    * removed admin committee and dependencies
    
    * Revert "removed admin committee and dependencies"
    
    This reverts commit 8e7dc2198002b79cdec250ab50b1075ec2cc5fdc.
    
    * changed infra committee
    
    * removed extraneous text
    
    * addressed multiple issues
    
    * corrected emails
    
    * updated corporate email on sponsors page within text

--- model/sponsors.go
+++ model/sponsors.go
@@ -2,6 +2,8 @@ package model
 
 type Sponsors struct {
 	Pitch       string               `yaml:"pitch"`
+	Contact     string               `yaml:"contact"`
+	Email     string                 `yaml:"email"`
 	GetInvolved string               `yaml:"getInvolved"`
 	Packages    []SponsorshipPackage `yaml:"packages"`
 }
