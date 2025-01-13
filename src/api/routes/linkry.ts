04bc57da8e550b3f34bbff682e8a2e54a7ed4cb5
Dev Singh <dsingh14@illinois.edu>
2025-01-12 20:06:27 -0600

    setup the redirect route

--- src/api/routes/linkry.ts
+++ src/api/routes/linkry.ts
@@ -1,9 +1,17 @@
 import { FastifyPluginAsync } from "fastify";
 import { z } from "zod";
 import { AppRoles } from "../../common/roles.js";
-import { NotImplementedError } from "../../common/errors/index.js";
+import {
+  BaseError,
+  DatabaseFetchError,
+  NotFoundError,
+  NotImplementedError,
+} from "../../common/errors/index.js";
 import { intersection } from "../plugins/auth.js";
 import { NoDataRequest } from "../types.js";
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { environmentConfig, genericConfig } from "common/config.js";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 
 type LinkrySlugOnlyRequest = {
   Params: { id: string };
@@ -13,12 +21,12 @@ type LinkrySlugOnlyRequest = {
 
 const rawRequest = {
   slug: z.string().min(1),
-  full: z.string().url().min(1),
+  redirect: z.string().url().min(1),
   groups: z.optional(z.array(z.string()).min(1)),
 };
 
 const createRequest = z.object(rawRequest);
-const patchRequest = z.object({ ...rawRequest, slug: z.undefined() });
+const patchRequest = z.object({ redirect: z.string().url().min(1) });
 
 type LinkyCreateRequest = {
   Params: undefined;
@@ -32,9 +40,44 @@ type LinkryPatchRequest = {
   Body: z.infer<typeof patchRequest>;
 };
 
+const dynamoClient = new DynamoDBClient({
+  region: genericConfig.AwsRegion,
+});
+
 const linkryRoutes: FastifyPluginAsync = async (fastify, _options) => {
   fastify.get<LinkrySlugOnlyRequest>("/redir/:id", async (request, reply) => {
-    throw new NotImplementedError({});
+    const id = request.params.id;
+    const command = new QueryCommand({
+      TableName: genericConfig.LinkryDynamoTableName,
+      KeyConditionExpression:
+        "#slug = :slugVal AND begins_with(#access, :accessVal)",
+      ExpressionAttributeNames: {
+        "#slug": "slug",
+        "#access": "access",
+      },
+      ExpressionAttributeValues: {
+        ":slugVal": { S: id },
+        ":accessVal": { S: "OWNER#" },
+      },
+    });
+    try {
+      const result = await dynamoClient.send(command);
+      if (!result || !result.Items || result.Items.length === 0) {
+        return reply
+          .headers({ "content-type": "text/html" })
+          .status(404)
+          .sendFile("404.html");
+      }
+      return reply.redirect(unmarshall(result.Items[0]).redirect);
+    } catch (e) {
+      if (e instanceof BaseError) {
+        throw e;
+      }
+      request.log.error(e);
+      throw new DatabaseFetchError({
+        message: "Could not retrieve mapping, please try again later.",
+      });
+    }
   });
   fastify.post<LinkyCreateRequest>(
     "/redir",
@@ -68,6 +111,7 @@ const linkryRoutes: FastifyPluginAsync = async (fastify, _options) => {
     },
     async (request, reply) => {
       // make sure that a user can manage this link, either via owning or being in a group that has access to it, or is a LINKS_ADMIN.
+      // you can only change the URL it redirects to
       throw new NotImplementedError({});
     },
   );
