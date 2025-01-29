f0df3d7049d4675563c3add00d39db63f63a18e5
Dev Singh <dsingh14@illinois.edu>
2025-01-28 18:35:10 -0600

    weird hack around build process

---
+++ src/api/build.js
@@ -0,0 +1,35 @@
+import esbuild from "esbuild";
+
+esbuild
+  .build({
+    entryPoints: ["api/lambda.js"], // Entry file
+    bundle: true,
+    format: "esm",
+    minify: true,
+    outdir: "../../dist/lambda/",
+    outExtension: { ".js": ".mjs" },
+    loader: {
+      ".png": "file",
+      ".pkpass": "file",
+      ".json": "file",
+    }, // File loaders
+    target: "es2022", // Target ES2022
+    sourcemap: false,
+    platform: "node",
+    external: ["aws-sdk", "moment-timezone","passkit-generator", "fastify"],
+    banner: {
+      js: `
+        import path from 'path';
+        import { fileURLToPath } from 'url';
+        import { createRequire as topLevelCreateRequire } from 'module';
+        const require = topLevelCreateRequire(import.meta.url);
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+      `.trim(),
+    }, // Banner for compatibility with CommonJS
+  })
+  .then(() => console.log("Build completed successfully!"))
+  .catch((error) => {
+    console.error("Build failed:", error);
+    process.exit(1);
+  });
