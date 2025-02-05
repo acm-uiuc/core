db5353338c4afa5d1f78edf9ead1aafcb1a14883
Dev Singh <dsingh14@illinois.edu>
2025-02-05 11:16:35 -0600

    fix vitest resolution

--- tests/unit/vitest.config.ts
+++ tests/unit/vitest.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   test: {
     setupFiles: "./tests/unit/vitest.setup.ts",
+  },
+  resolve: {
+    alias: {
+      "api/": path.resolve(__dirname, "../../src/api/"),
+    },
   },
 });
