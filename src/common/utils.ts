aaba7051ca5158f3b6a06f48b98d9ee6c808592a
Dev Singh <dev@devksingh.com>
2025-02-11 06:37:39 +0000

    change default name transformation

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,9 +1,9 @@
 export function transformCommaSeperatedName(name: string) {
   if (name.includes(",")) {
     try {
-      const split = name.split(",")
-      return `${split[1].slice(1, split[1].length)} ${split[0]}`
-    } catch (e) {
+      const split = name.split(",");
+      return `${split[1].slice(1, split[1].length).split(" ")[0]} ${split[0]}`;
+    } catch {
       return name;
     }
   }
