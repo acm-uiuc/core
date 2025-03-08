e546840c0b50791ffe00aad7fc5da3eb19b9e803
Dev Singh <dsingh14@illinois.edu>
2025-03-08 15:37:33 -0600

    revert mobilewallet

--- src/api/routes/mobileWallet.ts
+++ src/api/routes/mobileWallet.ts
@@ -54,8 +54,9 @@ const mobileWalletRoute: FastifyPluginAsync = async (fastify, _options) => {
         });
       }
       const isPaidMember = await checkPaidMembership(
+        fastify.environmentConfig.MembershipApiEndpoint,
+        request.log,
         request.query.email.replace("@illinois.edu", ""),
-        fastify.dynamoClient,
       );
       if (!isPaidMember) {
         throw new UnauthenticatedError({
