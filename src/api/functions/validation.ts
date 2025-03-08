64dce2cefc3c82e773e8095bcd69aa1a39a97b73
Dev Singh <dsingh14@illinois.edu>
2025-03-08 14:48:06 -0600

    update netid validation regex

--- src/api/functions/validation.ts
+++ src/api/functions/validation.ts
@@ -7,6 +7,6 @@ export function validateEmail(email: string): boolean {
 }
 
 export function validateNetId(netId: string): boolean {
-  const regex = /^[a-zA-Z0-9\-]+$/;
+  const regex = /^[a-zA-Z]{2}[a-zA-Z\-]*(?:[2-9]|[1-9][0-9]{1,2})?$/;
   return regex.test(netId);
 }
