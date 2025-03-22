7e37db3a97c775049e49d5053910ff330fb2ad5c
Dev Singh <dsingh14@illinois.edu>
2025-03-22 10:34:42 -0500

    Better local logging and more tests (#81)
    * fix favicon missing log
    
    * fix test tsconfig
    
    * add a test for protected and tickets routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@
 import { randomUUID } from "crypto";
 import fastify, { FastifyInstance } from "fastify";
 import FastifyAuthProvider from "@fastify/auth";
+import fastifyStatic from "@fastify/static";
 import fastifyAuthPlugin from "./plugins/auth.js";
 import protectedRoute from "./routes/protected.js";
 import errorHandlerPlugin from "./plugins/errorHandler.js";
@@ -24,12 +25,13 @@ import { SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
 import mobileWalletRoute from "./routes/mobileWallet.js";
 import stripeRoutes from "./routes/stripe.js";
 import membershipPlugin from "./routes/membership.js";
+import path from "path"; // eslint-disable-line import/no-nodejs-modules
 
 dotenv.config();
 
 const now = () => Date.now();
 
-async function init() {
+async function init(prettyPrint: boolean = false) {
   const dynamoClient = new DynamoDBClient({
     region: genericConfig.AwsRegion,
   });
@@ -37,10 +39,21 @@ async function init() {
   const secretsManagerClient = new SecretsManagerClient({
     region: genericConfig.AwsRegion,
   });
-
+  const transport = prettyPrint
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "SYS:standard",
+          ignore: "pid,hostname",
+          singleLine: false,
+        },
+      }
+    : undefined;
   const app: FastifyInstance = fastify({
     logger: {
       level: process.env.LOG_LEVEL || "info",
+      transport,
     },
     rewriteUrl: (req) => {
       const url = req.url;
@@ -71,6 +84,10 @@ async function init() {
   await app.register(fastifyZodValidationPlugin);
   await app.register(FastifyAuthProvider);
   await app.register(errorHandlerPlugin);
+  await app.register(fastifyStatic, {
+    root: path.join(__dirname, "public"),
+    prefix: "/",
+  });
   if (!process.env.RunEnvironment) {
     process.env.RunEnvironment = "dev";
   }
@@ -104,7 +121,6 @@ async function init() {
     );
     done();
   });
-  app.get("/", (_, reply) => reply.send("Welcome to the ACM @ UIUC Core API!"));
   app.get("/api/v1/healthz", (_, reply) => reply.send({ message: "UP" }));
   await app.register(
     async (api, _options) => {
@@ -143,7 +159,7 @@ if (import.meta.url === `file://${process.argv[1]}`) {
     );
     process.exit(1);
   }
-  const app = await init();
+  const app = await init(true);
   app.listen({ port: 8080 }, async (err) => {
     /* eslint no-console: ["error", {"allow": ["log", "error"]}] */
     if (err) console.error(err);
---
+++ tests/live/protected.test.ts
@@ -0,0 +1,33 @@
+import { expect, test, describe } from "vitest";
+import { createJwt } from "./utils";
+import { allAppRoles } from "../../src/common/roles";
+
+const baseEndpoint = `https://core.aws.qa.acmuiuc.org`;
+
+describe("Role checking live API tests", async () => {
+  const token = await createJwt();
+  test(
+    "Test that auth is present on the GET route",
+    { timeout: 10000 },
+    async () => {
+      const response = await fetch(`${baseEndpoint}/api/v1/protected/`, {
+        method: "GET",
+      });
+      expect(response.status).toBe(403);
+    },
+  );
+  test("Test that getting metadata succeeds", { timeout: 10000 }, async () => {
+    const response = await fetch(`${baseEndpoint}/api/v1/protected`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    expect(response.status).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toStrictEqual({
+      username: "infra@acm.illinois.edu",
+      roles: allAppRoles,
+    });
+  });
+});
