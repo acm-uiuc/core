d362d020ef1210929a8186250b0bb6aa6873d636
Dev Singh <dsingh14@illinois.edu>
2025-03-26 13:23:23 -0500

    add ratelimiter to protected route

--- src/api/routes/protected.ts
+++ src/api/routes/protected.ts
@@ -1,11 +1,17 @@
 import { FastifyPluginAsync } from "fastify";
 import fastifyCaching from "@fastify/caching";
+import rateLimiter from "api/plugins/rateLimiter.js";
 
 const protectedRoute: FastifyPluginAsync = async (fastify, _options) => {
   fastify.register(fastifyCaching, {
     privacy: fastifyCaching.privacy.PRIVATE,
     serverExpiresIn: 0,
     expiresIn: 60 * 60 * 2,
+  });
+  await fastify.register(rateLimiter, {
+    limit: 15,
+    duration: 30,
+    rateLimitIdentifier: "protected",
   });
   fastify.get("/", async (request, reply) => {
     const roles = await fastify.authorize(request, reply, []);
