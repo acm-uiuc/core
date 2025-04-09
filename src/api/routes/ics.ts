2d7be7a6d4a6d05ecc3c4f9a05c43937a7f815b6
Dev Singh <dsingh14@illinois.edu>
2025-04-09 15:23:12 -0500

    Add ETAG header to ICS responses

--- src/api/routes/ics.ts
+++ src/api/routes/ics.ts
@@ -15,14 +15,15 @@ import ical, {
 import moment from "moment";
 import { getVtimezoneComponent } from "@touch4it/ical-timezones";
 import { OrganizationList } from "../../common/orgs.js";
-import { EventRepeatOptions } from "./events.js";
+import { CLIENT_HTTP_CACHE_POLICY, EventRepeatOptions } from "./events.js";
 import rateLimiter from "api/plugins/rateLimiter.js";
+import { getCacheCounter } from "api/functions/cache.js";
 
 const repeatingIcalMap: Record<EventRepeatOptions, ICalEventJSONRepeatingData> =
-  {
-    weekly: { freq: ICalEventRepeatingFreq.WEEKLY },
-    biweekly: { freq: ICalEventRepeatingFreq.WEEKLY, interval: 2 },
-  };
+{
+  weekly: { freq: ICalEventRepeatingFreq.WEEKLY },
+  biweekly: { freq: ICalEventRepeatingFreq.WEEKLY, interval: 2 },
+};
 
 function generateHostName(host: string) {
   if (host == "ACM" || !host) {
@@ -46,97 +47,116 @@ const icalPlugin: FastifyPluginAsync = async (fastify, _options) => {
       TableName: genericConfig.EventsDynamoTableName,
     };
     let response;
-    if (host) {
-      if (!OrganizationList.includes(host)) {
-        throw new ValidationError({
-          message: `Invalid host parameter "${host}" in path.`,
+    const ifNoneMatch = request.headers["if-none-match"];
+    if (ifNoneMatch) {
+      const etag = await getCacheCounter(
+        fastify.dynamoClient,
+        `events-etag-${host || "all"}`,
+      );
+
+      if (
+        ifNoneMatch === `"${etag.toString()}"` ||
+        ifNoneMatch === etag.toString()
+      ) {
+        return reply
+          .code(304)
+          .header("ETag", etag)
+          .header("Cache-Control", CLIENT_HTTP_CACHE_POLICY)
+          .send();
+      }
+
+      reply.header("etag", etag);
+      if (host) {
+        if (!OrganizationList.includes(host)) {
+          throw new ValidationError({
+            message: `Invalid host parameter "${host}" in path.`,
+          });
+        }
+        queryParams = {
+          ...queryParams,
+        };
+        response = await fastify.dynamoClient.send(
+          new QueryCommand({
+            ...queryParams,
+            ExpressionAttributeValues: {
+              ":host": {
+                S: host,
+              },
+            },
+            KeyConditionExpression: "host = :host",
+            IndexName: "HostIndex",
+          }),
+        );
+      } else {
+        response = await fastify.dynamoClient.send(new ScanCommand(queryParams));
+      }
+      const dynamoItems = response.Items
+        ? response.Items.map((x) => unmarshall(x))
+        : null;
+      if (!dynamoItems) {
+        throw new NotFoundError({
+          endpointName: host ? `/api/v1/ical/${host}` : "/api/v1/ical",
         });
       }
-      queryParams = {
-        ...queryParams,
-      };
-      response = await fastify.dynamoClient.send(
-        new QueryCommand({
-          ...queryParams,
-          ExpressionAttributeValues: {
-            ":host": {
-              S: host,
-            },
-          },
-          KeyConditionExpression: "host = :host",
-          IndexName: "HostIndex",
-        }),
-      );
-    } else {
-      response = await fastify.dynamoClient.send(new ScanCommand(queryParams));
-    }
-    const dynamoItems = response.Items
-      ? response.Items.map((x) => unmarshall(x))
-      : null;
-    if (!dynamoItems) {
-      throw new NotFoundError({
-        endpointName: host ? `/api/v1/ical/${host}` : "/api/v1/ical",
+      // generate friendly calendar name
+      let calendarName =
+        host && host.includes("ACM")
+          ? `${host} Events`
+          : `ACM@UIUC - ${host} Events`;
+      if (host == "ACM") {
+        calendarName = "ACM@UIUC - Major Events";
+      }
+      if (!host) {
+        calendarName = "ACM@UIUC - All Events";
+      }
+      const calendar = ical({ name: calendarName });
+      calendar.timezone({
+        name: "America/Chicago",
+        generator: getVtimezoneComponent,
       });
-    }
-    // generate friendly calendar name
-    let calendarName =
-      host && host.includes("ACM")
-        ? `${host} Events`
-        : `ACM@UIUC - ${host} Events`;
-    if (host == "ACM") {
-      calendarName = "ACM@UIUC - Major Events";
-    }
-    if (!host) {
-      calendarName = "ACM@UIUC - All Events";
-    }
-    const calendar = ical({ name: calendarName });
-    calendar.timezone({
-      name: "America/Chicago",
-      generator: getVtimezoneComponent,
-    });
-    calendar.method(ICalCalendarMethod.PUBLISH);
-    for (const rawEvent of dynamoItems) {
-      let event = calendar.createEvent({
-        start: moment.tz(rawEvent.start, "America/Chicago"),
-        end: rawEvent.end
-          ? moment.tz(rawEvent.end, "America/Chicago")
-          : moment.tz(rawEvent.start, "America/Chicago"),
-        summary: rawEvent.title,
-        description: rawEvent.locationLink
-          ? `Host: ${rawEvent.host}\nGoogle Maps Link: ${rawEvent.locationLink}\n\n` +
+      calendar.method(ICalCalendarMethod.PUBLISH);
+      for (const rawEvent of dynamoItems) {
+        let event = calendar.createEvent({
+          start: moment.tz(rawEvent.start, "America/Chicago"),
+          end: rawEvent.end
+            ? moment.tz(rawEvent.end, "America/Chicago")
+            : moment.tz(rawEvent.start, "America/Chicago"),
+          summary: rawEvent.title,
+          description: rawEvent.locationLink
+            ? `Host: ${rawEvent.host}\nGoogle Maps Link: ${rawEvent.locationLink}\n\n` +
             rawEvent.description
-          : `Host: ${rawEvent.host}\n\n` + rawEvent.description,
-        timezone: "America/Chicago",
-        organizer: generateHostName(host),
-        id: rawEvent.id,
-      });
+            : `Host: ${rawEvent.host}\n\n` + rawEvent.description,
+          timezone: "America/Chicago",
+          organizer: generateHostName(host),
+          id: rawEvent.id,
+        });
 
-      if (rawEvent.repeats) {
-        if (rawEvent.repeatEnds) {
-          event = event.repeating({
-            ...repeatingIcalMap[rawEvent.repeats as EventRepeatOptions],
-            until: moment.tz(rawEvent.repeatEnds, "America/Chicago"),
+        if (rawEvent.repeats) {
+          if (rawEvent.repeatEnds) {
+            event = event.repeating({
+              ...repeatingIcalMap[rawEvent.repeats as EventRepeatOptions],
+              until: moment.tz(rawEvent.repeatEnds, "America/Chicago"),
+            });
+          } else {
+            event.repeating(
+              repeatingIcalMap[rawEvent.repeats as EventRepeatOptions],
+            );
+          }
+        }
+        if (rawEvent.location) {
+          event = event.location({
+            title: rawEvent.location,
           });
-        } else {
-          event.repeating(
-            repeatingIcalMap[rawEvent.repeats as EventRepeatOptions],
-          );
         }
       }
-      if (rawEvent.location) {
-        event = event.location({
-          title: rawEvent.location,
-        });
-      }
-    }
 
-    reply
-      .headers({
-        "Content-Type": "text/calendar; charset=utf-8",
-        "Content-Disposition": 'attachment; filename="calendar.ics"',
-      })
-      .send(calendar.toString());
-  });
+      reply
+        .headers({
+          "Content-Type": "text/calendar; charset=utf-8",
+          "Content-Disposition": 'attachment; filename="calendar.ics"',
+        })
+        .send(calendar.toString());
+    });
 };
 
 export default icalPlugin;
