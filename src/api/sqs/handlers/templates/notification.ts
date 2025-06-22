96cafd249f1cce680a07044fd371740630be894d
Dev Singh <dev@devksingh.com>
2025-06-22 13:18:37 -0400

    run the build at unit test time

--- src/api/sqs/handlers/templates/notification.ts
+++ src/api/sqs/handlers/templates/notification.ts
@@ -15,82 +15,35 @@ const template = /*html*/ `
         margin: 0;
         color: #444;
       }
-      pre {
-        background: #f4f4f4f4;
-        padding: 2px;
-      }
-      table {
-        width: 100%;
-        border: 1px solid #ddd;
-      }
-      table td {
-        border-color: #ddd;
-        padding: 5px;
-      }
-      .wrap {
-        background-color: #fff;
-        padding: 30px;
-        max-width: 525px;
-        margin: 0 auto;
-        border-radius: 5px;
-      }
-      .button {
-        background: #0055d4;
-        border-radius: 3px;
-        text-decoration: none !important;
-        color: #fff !important;
-        font-weight: bold;
-        padding: 10px 30px;
-        display: inline-block;
-      }
-      .button:hover {
-        background: #111;
-      }
-      .footer {
-        text-align: center;
-        font-size: 12px;
-        color: #888;
-      }
-      .footer a {
-        color: #888;
-        margin-right: 5px;
-      }
-      .gutter {
-        padding: 30px;
-      }
-      img {
-        max-width: 100%;
-        height: auto;
-      }
-      a {
-        color: #0055d4;
-      }
-      a:hover {
-        color: #111;
-      }
-      @media screen and (max-width: 600px) {
-        .wrap {
-          max-width: auto;
-        }
-        .gutter {
-          padding: 10px;
-        }
-      }
     </style>
   </head>
-  <body style="background-color: #F0F1F3;font-family: 'Helvetica Neue', 'Segoe UI', Helvetica, sans-serif;font-size: 15px;line-height: 26px;margin: 0;color: #444; display: flex; justify-content: center; flex-direction: column">
-    <div class="gutter" style="padding: 30px;">&nbsp;</div>
-    <img src="https://static.acm.illinois.edu/banner-blue.png" style="height: 100px; width: 210px; align-self: center;"/>
-    <br />
-    <div class="wrap" style="background-color: #fff;padding: 30px;max-width: 525px;margin: 0 auto;border-radius: 5px;">
-      {{nl2br content}}
-    </div>
-    <div class="footer" style="text-align: center;font-size: 12px;color: #888;">
-      <p>
-        <a href="https://acm.illinois.edu" style="color: #888;">ACM @ UIUC Homepage</a>
-        <a href="mailto:officers@acm.illinois.edu" style="color: #888;">Email ACM @ UIUC</a>
-      </p>
-    </div>
+  <body style="background-color: #F0F1F3;font-family: 'Helvetica Neue', 'Segoe UI', Helvetica, sans-serif;font-size: 15px;line-height: 26px;margin: 0;color: #444;">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" style="background-color: #F0F1F3;">
+      <tr>
+        <td align="center" style="padding: 30px;">
+          <table width="525" border="0" cellpadding="0" cellspacing="0" style="width: 525px; max-width: 525px; background-color: #ffffff; border-radius: 5px;">
+            <tr>
+              <td align="center" style="padding-top: 30px;">
+                <img src="https://static.acm.illinois.edu/banner-blue.png" style="height: 100px; width: 210px; display: block; margin: 0 auto;" alt="ACM UIUC Logo"/>
+              </td>
+            </tr>
+            <tr>
+              <td style="padding: 30px;">
+                {{nl2br content}}
+              </td>
+            </tr>
+            <tr>
+              <td align="center" style="padding-bottom: 30px;">
+                <p style="font-size: 12px; color: #888; text-align: center;">
+                  <a href="https://acm.illinois.edu" style="color: #888;">ACM @ UIUC Homepage</a>
+                  <a href="mailto:officers@acm.illinois.edu" style="color: #888; margin-left: 5px;">Email ACM @ UIUC</a>
+                </p>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
   </body>
 </html>
 `;
