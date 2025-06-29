f6d759086aab6b7ef0b6c439297fdad572a5c4c3
Dev Singh <dev@devksingh.com>
2025-06-29 17:51:38 -0500

    fix remove error

--- src/api/routes/iam.ts
+++ src/api/routes/iam.ts
@@ -399,7 +399,7 @@ const iamRoutes: FastifyPluginAsync = async (fastify, _options) => {
               entry: {
                 module: Modules.IAM,
                 actor: request.username!,
-                target: request.body.add[i],
+                target: request.body.remove[i],
                 message: `remove target from group ID ${groupId}`,
                 requestId: request.id,
               },
@@ -412,7 +412,7 @@ const iamRoutes: FastifyPluginAsync = async (fastify, _options) => {
               entry: {
                 module: Modules.IAM,
                 actor: request.username!,
-                target: request.body.add[i],
+                target: request.body.remove[i],
                 message: `failed to remove target from group ID ${groupId}`,
                 requestId: request.id,
               },
@@ -420,12 +420,12 @@ const iamRoutes: FastifyPluginAsync = async (fastify, _options) => {
           );
           if (result.reason instanceof EntraGroupError) {
             response.failure.push({
-              email: request.body.add[i],
+              email: request.body.remove[i],
               message: result.reason.message,
             });
           } else {
             response.failure.push({
-              email: request.body.add[i],
+              email: request.body.remove[i],
               message: "An unknown error occurred.",
             });
           }
