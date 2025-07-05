518f4d9544eccb51fd5eaa1049755d2e472166db
Dev Singh <dev@devksingh.com>
2025-07-05 14:07:42 -0500

    add preflight CORS live tests

--- tests/live/cors.test.ts
+++ tests/live/cors.test.ts
@@ -4,6 +4,19 @@ import { getBaseEndpoint } from "./utils.js";
 const baseEndpoint = getBaseEndpoint();
 
 describe("CORS tests", async () => {
+  test("Events: Known URL is preflight allowed in CORS", async () => {
+    const response = await fetch(`${baseEndpoint}/api/v1/events`, {
+      method: "OPTIONS",
+      headers: {
+        "Access-Control-Request-Method": "GET",
+        Origin: "https://acmuiuc.pages.dev",
+      },
+    });
+    expect(response.status).toBe(204);
+    expect(response.headers.get("access-control-allow-origin")).toStrictEqual(
+      "https://acmuiuc.pages.dev",
+    );
+  });
   test("Events: Known URL is allowed in CORS", async () => {
     const response = await fetch(`${baseEndpoint}/api/v1/events`, {
       headers: {
@@ -14,6 +27,17 @@ describe("CORS tests", async () => {
     expect(response.headers.get("access-control-allow-origin")).toStrictEqual(
       "https://acmuiuc.pages.dev",
     );
+  });
+  test("Events: Unknown URL is preflight not allowed in CORS", async () => {
+    const response = await fetch(`${baseEndpoint}/api/v1/events`, {
+      method: "OPTIONS",
+      headers: {
+        "Access-Control-Request-Method": "GET",
+        Origin: "https://google.com",
+      },
+    });
+    expect(response.status).toBe(204);
+    expect(response.headers).not.toHaveProperty("access-control-allow-origin");
   });
   test("Events: Unknown URL is not allowed in CORS", async () => {
     const response = await fetch(`${baseEndpoint}/api/v1/events`, {
@@ -35,6 +59,19 @@ describe("CORS tests", async () => {
       "https://acmuiuc.pages.dev",
     );
   });
+  test("Membership: Known URL is preflight allowed in CORS", async () => {
+    const response = await fetch(`${baseEndpoint}/api/v1/membership/zzzzzz`, {
+      method: "OPTIONS",
+      headers: {
+        "Access-Control-Request-Method": "GET",
+        Origin: "https://acmuiuc.pages.dev",
+      },
+    });
+    expect(response.status).toBe(204);
+    expect(response.headers.get("access-control-allow-origin")).toStrictEqual(
+      "https://acmuiuc.pages.dev",
+    );
+  });
   test("Membership: Unknown URL is not allowed in CORS", async () => {
     const response = await fetch(`${baseEndpoint}/api/v1/membership/zzzzzz`, {
       headers: {
@@ -42,6 +79,17 @@ describe("CORS tests", async () => {
       },
     });
     expect(response.status).toBe(200);
+    expect(response.headers).not.toHaveProperty("access-control-allow-origin");
+  });
+  test("Membership: Unknown URL is preflight not allowed in CORS", async () => {
+    const response = await fetch(`${baseEndpoint}/api/v1/membership/zzzzzz`, {
+      method: "OPTIONS",
+      headers: {
+        "Access-Control-Request-Method": "GET",
+        Origin: "https://google.com",
+      },
+    });
+    expect(response.status).toBe(204);
     expect(response.headers).not.toHaveProperty("access-control-allow-origin");
   });
 });
