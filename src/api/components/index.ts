ffe37ee103930f409f632b3ffbd151005544be93
Dev Singh <dev@devksingh.com>
2025-07-11 12:48:17 -0400

    update schemas

--- src/api/components/index.ts
+++ src/api/components/index.ts
@@ -1,8 +1,11 @@
 import { AppRoles } from "common/roles.js";
 import { FastifyZodOpenApiSchema } from "fastify-zod-openapi";
 import * as z from "zod/v4";
-import { validateNetId } from "api/functions/validation.js";
 import { CoreOrganizationList } from "@acm-uiuc/js-shared";
+export {
+  illinoisSemesterId as semesterId,
+  illinoisNetId,
+} from "../../common/types/generic.js";
 
 export const ts = z.coerce.number().min(0).optional().meta({
   description:
@@ -17,34 +20,12 @@ export const groupId = z.string().min(1).meta({
   id: "EntraGroupId",
 });
 
-export const illinoisNetId = z
-  .string()
-  .min(3, { message: "NetID must be at least 3 characters." })
-  .max(8, { message: "NetID cannot be more than 8 characters." })
-  .regex(/^[a-zA-Z]{2}[a-zA-Z-]*(?:[2-9]|[1-9][0-9]{1,2})?$/, {
-    message: "NetID is not valid!",
-  })
-  .meta({
-    description: "Valid Illinois NetID",
-    example: "rjjones",
-    id: "IllinoisNetId",
-  });
-
 export const acmCoreOrganization = z
   .enum(CoreOrganizationList as [string, ...string[]])
   .meta({
     description: "ACM Organization",
     id: "AcmOrganization",
     examples: ["ACM", "Infrastructure Committee"],
-  });
-
-export const semesterId = z
-  .string()
-  .min(1)
-  .meta({
-    description: "Short semester slug for a given semester.",
-    id: "IllinoisSemesterId",
-    examples: ["sp25", "fa24"],
   });
 
 export function withTags<T extends FastifyZodOpenApiSchema>(
