18d551dc4c4032308bd59340bd34e3956815c4b0
Dev Singh <dsingh14@illinois.edu>
2025-07-22 10:09:32 -0400

    alarms to terraform

---
+++ terraform/modules/alarms/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_prefix" {
+  type        = string
+  description = "Prefix before each resource"
+}
+
+variable "priority_sns_arn" {
+  type        = string
+  description = "Priority SNS alerts ARN"
+}
+
+variable "standard_sns_arn" {
+  type        = string
+  description = "Standard SNS alerts ARN"
+}
+
+variable "main_cloudfront_distribution_id" {
+  type        = string
+  description = "ID for the cloudfront distribution that serves the main application"
+}
