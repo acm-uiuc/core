e499df02179958ffa2f130f729150eafb6362c4f
Dev Singh <dsingh14@illinois.edu>
2025-07-24 19:18:37 -0400

    Import resources into terraform

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -24,7 +24,8 @@ provider "aws" {
   region = "us-east-1"
   default_tags {
     tags = {
-      project = var.ProjectId
+      project           = var.ProjectId
+      terraform_managed = true
     }
   }
 }
@@ -122,5 +123,71 @@ resource "aws_dynamodb_table" "api_keys" {
   ttl {
     attribute_name = "expiresAt"
     enabled        = true
+  }
+}
+
+// Room Requests
+import {
+  to = aws_dynamodb_table.room_requests
+  id = "${var.ProjectId}-room-requests"
+}
+import {
+  to = aws_dynamodb_table.room_requests_status
+  id = "${var.ProjectId}-room-requests-status"
+}
+resource "aws_dynamodb_table" "room_requests" {
+  billing_mode                = "PAY_PER_REQUEST"
+  name                        = "${var.ProjectId}-room-requests"
+  deletion_protection_enabled = true
+  hash_key                    = "semesterId"
+  range_key                   = "userId#requestId"
+  point_in_time_recovery {
+    enabled = true
+  }
+  attribute {
+    name = "userId#requestId"
+    type = "S"
+  }
+  attribute {
+    name = "requestId"
+    type = "S"
+  }
+  attribute {
+    name = "semesterId"
+    type = "S"
+  }
+  global_secondary_index {
+    name            = "RequestIdIndex"
+    hash_key        = "requestId"
+    projection_type = "ALL"
+  }
+}
+
+resource "aws_dynamodb_table" "room_requests_status" {
+  billing_mode                = "PAY_PER_REQUEST"
+  name                        = "${var.ProjectId}-room-requests-status"
+  deletion_protection_enabled = true
+  hash_key                    = "requestId"
+  range_key                   = "createdAt#status"
+  point_in_time_recovery {
+    enabled = true
+  }
+  attribute {
+    name = "createdAt#status"
+    type = "S"
+  }
+  attribute {
+    name = "requestId"
+    type = "S"
+  }
+  attribute {
+    name = "semesterId"
+    type = "S"
+  }
+  global_secondary_index {
+    name            = "SemesterId"
+    hash_key        = "semesterId"
+    range_key       = "requestId"
+    projection_type = "ALL"
   }
 }
