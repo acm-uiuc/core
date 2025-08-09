47f62bedfe6073068668b62289f5509d1ba06377
Dev Singh <dsingh14@illinois.edu>
2025-08-09 09:56:36 -0500

    update terraform

--- terraform/envs/qa/main.tf
+++ terraform/envs/qa/main.tf
@@ -82,6 +82,7 @@ module "frontend" {
   source             = "../../modules/frontend"
   BucketPrefix       = local.bucket_prefix
   CoreLambdaHost     = module.lambdas.core_function_url
+  CoreSlowLambdaHost = module.lambdas.core_slow_function_url
   OriginVerifyKey    = random_password.origin_verify_key.result
   ProjectId          = var.ProjectId
   CoreCertificateArn = var.CoreCertificateArn
