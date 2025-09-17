fb2d3f49d3d8bb384795c6689e5bdd0342182f4a
Dev Singh <dsingh14@illinois.edu>
2025-09-17 00:03:08 -0500

    Create a DynamoDB table for SIG info

--- terraform/modules/dynamo/main.tf
+++ terraform/modules/dynamo/main.tf
@@ -300,3 +300,17 @@ resource "aws_dynamodb_table" "cache" {
     enabled        = true
   }
 }
+
+resource "aws_dynamodb_table" "cache" {
+  billing_mode                = "PAY_PER_REQUEST"
+  name                        = "${var.ProjectId}-sigs"
+  deletion_protection_enabled = true
+  hash_key                    = "primaryKey"
+  point_in_time_recovery {
+    enabled = false
+  }
+  attribute {
+    name = "primaryKey"
+    type = "S"
+  }
+}
