8d477506a1d18ebf130a80529c5397306cc340a4
Dev Singh <dev@devksingh.com>
2025-10-07 02:32:05 -0500

    correctly handle min

--- src/ui/util/index.ts
+++ src/ui/util/index.ts
@@ -1,18 +1,27 @@
 import { ACMOrganization } from "@acm-uiuc/js-shared";
 import { OrgRoleDefinition } from "@common/roles";
 
+export type NonEmptyArray<T> = [T, ...T[]];
+
+export function min<T>(items: NonEmptyArray<T>): T {
+  let currentMin: T = items[0];
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] < currentMin) {
+      currentMin = items[i];
+    }
+  }
+  return currentMin;
+}
+
 export function getPrimarySuggestedOrg(
   orgRoles: OrgRoleDefinition[] | null | undefined,
 ): ACMOrganization {
   if (!orgRoles || orgRoles.length === 0) {
     return "";
   }
-  const leadOrgs = orgRoles
-    .filter((x) => x.role === "LEAD")
-    .map((x) => x.org)
-    .sort();
+  const leadOrgs = orgRoles.filter((x) => x.role === "LEAD").map((x) => x.org);
   if (leadOrgs.length > 0) {
-    return leadOrgs[0];
+    return min(leadOrgs as NonEmptyArray<ACMOrganization>);
   }
   return "";
 }
