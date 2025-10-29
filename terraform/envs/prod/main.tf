4e6b73acb110be9ba9c1175c14a73b889d2b0bd8
Dev Singh <dev@devksingh.com>
2025-10-28 20:49:06 -0500

    Setup wiring

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -37,7 +37,12 @@ locals {
     main = module.sqs_queues.main_queue_arn
     sqs  = module.sqs_queues.sales_email_queue_arn
   }
+  queue_arns_usw2 = {
+    main = module.sqs_queues_usw2.main_queue_arn
+    sqs  = module.sqs_queues_usw2.sales_email_queue_arn
+  }
   DynamoReplicationRegions = toset(["us-west-2"])
+  deployment_env           = "prod"
 }
 
 module "sqs_queues" {
@@ -93,21 +98,7 @@ module "lambdas" {
   region                           = "us-east-2"
   source                           = "../../modules/lambdas"
   ProjectId                        = var.ProjectId
-  RunEnvironment                   = "prod"
-  CurrentOriginVerifyKey           = module.origin_verify.current_origin_verify_key
-  PreviousOriginVerifyKey          = module.origin_verify.previous_origin_verify_key
-  PreviousOriginVerifyKeyExpiresAt = module.origin_verify.previous_invalid_time
-  LogRetentionDays                 = var.LogRetentionDays
-  EmailDomain                      = var.EmailDomain
-}
-
-// Multi-Region Failover: US-West-2
-
-module "lambdas_usw2" {
-  region                           = "us-west-2"
-  source                           = "../../modules/lambdas"
-  ProjectId                        = var.ProjectId
-  RunEnvironment                   = "prod"
+  RunEnvironment                   = local.deployment_env
   CurrentOriginVerifyKey           = module.origin_verify.current_origin_verify_key
   PreviousOriginVerifyKey          = module.origin_verify.previous_origin_verify_key
   PreviousOriginVerifyKeyExpiresAt = module.origin_verify.previous_invalid_time
@@ -153,6 +144,39 @@ resource "aws_lambda_event_source_mapping" "queue_consumer" {
   function_name           = module.lambdas.core_sqs_consumer_lambda_arn
   function_response_types = ["ReportBatchItemFailures"]
 }
+
+// Multi-Region Failover: us-west-2
+
+module "lambdas_usw2" {
+  region                           = "us-west-2"
+  source                           = "../../modules/lambdas"
+  ProjectId                        = var.ProjectId
+  RunEnvironment                   = local.deployment_env
+  CurrentOriginVerifyKey           = module.origin_verify.current_origin_verify_key
+  PreviousOriginVerifyKey          = module.origin_verify.previous_origin_verify_key
+  PreviousOriginVerifyKeyExpiresAt = module.origin_verify.previous_invalid_time
+  LogRetentionDays                 = var.LogRetentionDays
+  EmailDomain                      = var.EmailDomain
+}
+
+module "sqs_queues_usw2" {
+  region                        = "us-west-2"
+  depends_on                    = [module.lambdas_usw2]
+  source                        = "../../modules/sqs"
+  resource_prefix               = var.ProjectId
+  core_sqs_consumer_lambda_name = module.lambdas_usw2.core_sqs_consumer_lambda_name
+}
+
+resource "aws_lambda_event_source_mapping" "queue_consumer_usw2" {
+  region                  = "us-west-2"
+  depends_on              = [module.lambdas_usw2, module.sqs_queues_usw2]
+  for_each                = local.queue_arns_usw2
+  batch_size              = 5
+  event_source_arn        = each.value
+  function_name           = module.lambdas_usw2.core_sqs_consumer_lambda_arn
+  function_response_types = ["ReportBatchItemFailures"]
+}
+
 
 // This section last: moved records into modules
 moved {
