c031c6ba846112c777601be80d56b228277a6f08
Dev Singh <dsingh14@illinois.edu>
2026-02-26 02:09:13 -0600

    fix

--- terraform/modules/certificateIssuer/main.tf
+++ terraform/modules/certificateIssuer/main.tf
@@ -8,9 +8,20 @@ resource "aws_kms_key" "primary" {
   customer_master_key_spec = "RSA_4096"
   is_enabled               = true
   deletion_window_in_days  = var.DeletionWindowInDays
+
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = []
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
   })
 }
 
