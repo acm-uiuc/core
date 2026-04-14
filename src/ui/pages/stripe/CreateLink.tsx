c8ad2f433d5a16dadb8f28326ece7a528f69afc0
Aditya Kshirsagar <aditya29@icloud.com>
2026-04-14 13:12:37 -0500

    removed bug on stripe link page where org selector would not appear

--- src/ui/pages/stripe/CreateLink.tsx
+++ src/ui/pages/stripe/CreateLink.tsx
@@ -105,7 +105,7 @@ export const StripeCreateLinkPanel: React.FC<StripeCreateLinkPanelProps> = ({
               const primOrg = getPrimarySuggestedOrg(orgRoles);
               if (primOrg) {
                 setUserPrimaryOrg(primOrg);
-                form.setFieldValue("host", primOrg);
+                form.setFieldValue("acmOrg", primOrg);
               }
             }
           }}
